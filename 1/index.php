9b3c0ea82fab14aa3b8ab473db10c7b160cc7358
Ibatullina-DEV <81437438+Ibatullina-DEV@users.noreply.github.com>
2022-03-31 17:42:14 +0500

    Добавила файлы первой лабораторной работы

---
+++ 1/index.php
@@ -0,0 +1,47 @@
+<html>
+
+<head> <title> Лаба 1 </title> </head>
+<style>
+   body{
+    background-color: #C0D7E6; /* Цвет фона веб-страницы */
+   } 
+   li{
+  position: relative;
+  left: 500px;;
+    }
+    h2{
+       position: relative;
+  left: 460px;; 
+    }
+  </style>
+  <a href="http://a0654147.xsph.ru/">Вернуться назад</a>
+<div align="center">
+  <h1>Ибатуллина А.И. Пи-323</h1>  
+</div>
+<body>
+
+<u1>
+        <h2>Часть 1. Введение в PHP</h2>
+        <li><a href="http://a0654147.xsph.ru/1/lab-1-1.php">Лаба-1-1</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-2.php">Лаба-1-2</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-3.php">Лаба-1-3</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-4.php">Лаба-1-4</a></li>
+		<h2>Сам. работа № 1.</h2>
+        <li><a href="http://a0654147.xsph.ru/1/lab-1-5.php">Лаба-1-5 Варианты 14 и 2</a></li>
+        <h2>Часть 2. Управляющие конструкции</h2>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-6.php">Лаба-1-6</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-7.php">Лаба-1-7</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-8.php">Лаба-1-8</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-9.php">Лаба-1-9</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-10.php">Лаба-1-10</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-11.php">Лаба-1-11</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-12.php">Лаба-1-12</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-13.php">Лаба-1-13</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-14.php">Лаба-1-14</a></li>
+		<h2>Сам. работа  № 2. Действия с числами.</h2>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-15.php">Лаба-2-10</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-17.php">Лаба-2-11 (4 задача)</a></li>
+		<li><a href="http://a0654147.xsph.ru/1/lab-1-16.php">Лаба-2-11 (7 задача)</a></li>
+</u1>
+</body>
+</html>
