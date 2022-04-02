446d15c8b20243706eca4252f0faa8ffc48e1a94
Ibatullina-DEV <81437438+Ibatullina-DEV@users.noreply.github.com>
2022-04-02 22:36:39 +0500

    добавила файлы второй лабораторной работы

---
+++ 2/index.php
@@ -0,0 +1,37 @@
+<html>
+
+<head> <title> Лаба 2 </title> </head>
+<style>
+   body{
+    background-color: #C0D7E6; /* Цвет фона веб-страницы */
+   } 
+   li{
+  position: relative;
+  left: 500px;;
+    }
+    h2{
+       position: relative;
+  left: 460px;; 
+    }
+  </style>
+  <a href="http://a0654147.xsph.ru/">Вернуться назад</a>
+<div align="center">
+  <h1>Ибатуллина А.И. Пи-323</h1>  
+</div>
+<body>
+
+<u1>
+         <h2>Часть 1 - Работа с массивами</h2>
+        <li><a href="http://a0654147.xsph.ru/2/lab-2-1.php">Лаба-2-1</a></li>
+		<li><a href="http://a0654147.xsph.ru/2/lab-2-2.php">Лаба-2-2</a></li>
+		<li><a href="http://a0654147.xsph.ru/2/lab-2-3.php">Лаба-2-3</a></li>
+		<h2>САМОСТОЯТЕЛЬНАЯ РАБОТА</h2>
+		<li><a href="http://a0654147.xsph.ru/2/lab-2-4.php">Лаба-2-4</a></li>
+		<h2>Часть 2 - Функции пользователя</h2>
+        <li><a href="http://a0654147.xsph.ru/2/lab-2-5.php">Лаба-2-5</a></li>
+        <li><a href="http://a0654147.xsph.ru/2/lab-2-6.php">Лаба-2-6</a></li>
+		
+</u1>
+
+</body>
+</html>
