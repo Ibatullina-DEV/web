f17b2f0eea2b1ab3b6aeda222737fe93fdb2880f
Ibatullina-DEV <81437438+Ibatullina-DEV@users.noreply.github.com>
2022-04-02 23:42:52 +0500

    Добавила файл навигации

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+    <head> <title> Лабораторные работы</title> </head>
+<style>
+   body{
+    background-color: #C0D7E6; /* Цвет фона веб-страницы */
+   } 
+   li{
+  position: relative;
+  left: 500px;;
+    }
+  </style>
+  <div align="center">
+      <h1>Лабораторные работы - Ибатуллина А.И. Пи-323</h1><p>
+  </div>
+  
+
+<body>
+
+<p>
+<u1>
+        <li><a href="http://a0654147.xsph.ru/1/index.php">Лаб. работа 1.</a></li>
+       
+        <li><a href="http://a0654147.xsph.ru/2/index.php">Лаб. работа 2</a></li>
+        <li><a href="http://a0654147.xsph.ru/3/index.php">Лаб. работа 3 - Обработка данных форм</a></li>
+        <li><a href="http://a0654147.xsph.ru/4-5/index.php">Лаб. работа 4-5 - Базы данных MySQL и PHP</a></li>
+		<li><a href="http://a0654147.xsph.ru/6/index.php">Лаб. работа 6 - Сессии</a></li>
+
+</u1>
+</html>
